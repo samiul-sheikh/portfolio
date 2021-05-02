c0bf8c506b576cf766fe266719d8497375f90809
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-05-02 18:13:35 +0600

    display some project

--- src/Components/Projects/ProjectDetail/ProjectDetail.js
+++ src/Components/Projects/ProjectDetail/ProjectDetail.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import './ProjectDetail.css';
+import sheikhHome from '../../../Images/sheikh-home.PNG';
+import fashionHome from '../../../Images/fashion-home.PNG';
+import sportsHome from '../../../Images/sports-home.PNG';
 
-const ProjectDetail = ({ project }) => {
+const ProjectDetail = () => {
 
     return (
-        <div className="single-project" style={{border: '1px solid red'}}>
-            <img style={{height: '200px'}} src={project.img} alt="" />
-            <h2>{project.name}</h2>
+        <div className="mt-5">
+            <div className="mt-5">
+                <div className="row d-flex align-items-center" style={{ border: '1px solid red' }}>
+                    <div className="col-md-4">
+                        <img className="img-fluid" src={sheikhHome} alt="" />
+                    </div>
+                    <div className="col-md-6">
+                        <h2>Sheikh</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt suscipit quas sequi harum laborum illum iusto cupiditate sint perspiciatis ut?</p>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-5">
+                <div className="row d-flex align-items-center" style={{ border: '1px solid red' }}>
+                    <div className="col-md-4">
+                        <img className="img-fluid" src={fashionHome} alt="" />
+                    </div>
+                    <div className="col-md-6">
+                        <h2>Hello</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt suscipit quas sequi harum laborum illum iusto cupiditate sint perspiciatis ut?</p>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-5">
+                <div className="row d-flex align-items-center" style={{ border: '1px solid red' }}>
+                    <div className="col-md-4">
+                        <img className="img-fluid" src={sportsHome} alt="" />
+                    </div>
+                    <div className="col-md-6">
+                        <h2>Hello</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt suscipit quas sequi harum laborum illum iusto cupiditate sint perspiciatis ut?</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Projects/Projects/Projects.js
+++ src/Components/Projects/Projects/Projects.js
@@ -1,37 +1,17 @@
 import React from 'react';
-import sheikhHome from '../../../Images/sheikh-home.PNG';
-import fashionHome from '../../../Images/fashion-home.PNG';
-import sportsHome from '../../../Images/sports-home.PNG';
 import ProjectDetail from '../ProjectDetail/ProjectDetail';
-
-const projectData = [
-    {
-        name: 'Sheikh Technology',
-        img: sheikhHome
-    },
-    {
-        name: 'Fashion House',
-        img: fashionHome
-    },
-    {
-        name: 'Sports Team',
-        img: sportsHome
-    }
-]
 
 const Projects = () => {
     return (
-        <section>
+        <div>
             <div className="text-center">
                 <h2>My Projects</h2>
                 <h5>Here some of my successful projects.</h5>
             </div>
-            <div className="container m-auto">
-                {
-                    projectData.map(project => <ProjectDetail project={project}></ProjectDetail>)
-                }
+            <div className="container">
+                <ProjectDetail></ProjectDetail>
             </div>
-        </section>
+        </div>
     );
 };
 
