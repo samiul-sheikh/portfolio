555587b585341ab4ffcc4e8aee4bbc18919d6f56
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-05-02 07:28:14 +0600

    receive contact info in email using formspree

--- src/Components/ContactMe/ContactMe.js
+++ src/Components/ContactMe/ContactMe.js
@@ -7,21 +7,21 @@ const ContactMe = () => {
                 <h3>Contact Me</h3>
             </div>
             <div className="col-md-9 mx-auto">
-                <form action="">
+                <form action="https://formspree.io/f/xzbydgoq" method="POST">
                     <div className="form-group">
                         <h6>Your Name</h6>
                         <input type="text" className="form-control" name="name" />
                     </div>
                     <div className="form-group">
                         <h6>Your Email</h6>
-                        <input type="text" className="form-control" name="email" />
+                        <input type="email" className="form-control" name="email" />
                     </div>
                     <div className="form-group">
                         <h6>Your Message</h6>
                         <textarea type="text" className="form-control" name="message" rows="10" col="30"></textarea>
                     </div>
-                    <div className="form-group text-center">
-                        <button type="button" className="btn btn-info">Submit</button>
+                    <div className="text-center">
+                        <button type="submit" className="btn btn-info">Submit</button>
                     </div>
                 </form>
             </div>
