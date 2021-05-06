be72bccf9e8f59e6616f6559cc19371f6d666a42
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-05-06 23:26:03 +0600

    display project and blog in homepage

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Blog from '../../Blog/Blog';
+import ContactMe from '../../ContactMe/ContactMe';
 import Header from '../../Header/Header';
+import Projects from '../../Projects/Projects/Projects';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
+            <Projects></Projects>
+            <Blog></Blog>
+            <ContactMe></ContactMe>
         </div>
     );
 };
